a0690cfa3737a403bc2a73d5799b1197a6bd53e4
Azevedo Rayldo <azevedorayldo@gmail.com>
2022-05-13 12:56:00 -0300

    listagem completa

--- src/components/listuser/Listuser.js
+++ src/components/listuser/Listuser.js
@@ -3,79 +3,54 @@ import { DataGrid } from '@mui/x-data-grid';
 import ApiService from "../../service/ApiService";
 import { useState, useEffect } from "react";
 
-
-
-
 const columns = [
     { field: 'id', headerName: 'ID', width: 90 },
     {
-        field: 'firstName',
-        headerName: 'First name',
+        field: 'nome',
+        headerName: 'nome',
         width: 150,
         editable: true,
     },
     {
-        field: 'lastName',
-        headerName: 'Last name',
+        field: 'linha',
+        headerName: 'linha',
         width: 150,
         editable: true,
     },
     {
-        field: 'age',
-        headerName: 'Age',
+        field: 'validade',
+        headerName: 'validade',
         type: 'number',
-        width: 110,
+        width: 120,
         editable: true,
     },
     {
-        field: 'fullName',
-        headerName: 'Full name',
-        description: 'This column has a value getter and is not sortable.',
-        sortable: false,
-        width: 160,
-        valueGetter: (params) =>
-            `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+        field: 'valor',
+        headerName: 'valor',
+        type: 'number',
+        width: 120,
+        editable: true,
     },
+    
 ];
 
-const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
+const ListProduto = () => {
 
+        const [produtos, setProdutos] = useState([]);     
 
-const ListUser = () => {
-
-        const [produtos, setProduto] = useState(['']);
-
-        useEffect( () => {
-            ApiService.listarUsuarios().then(res =>{
-                setProduto(res.data);
-            })
-            console.log(" => ", produtos)
-        },[produtos]);
-
-        // produtos.map( (produto, key)=> {
-        //     columns[key] = [{
-        //         nome : produto.name
-        //     }]
-            
-        // });
-
+        useEffect(() => {
+            ApiService.listarUsuarios().then((res) => {
+              setProdutos(res.data.map(({ id, nome, linha, validade, valor  }) => ({ id, nome, linha, validade, valor })));
+            });
         
-
+            return setProdutos([]);
+          }, []);     
+        
     return (
-        <div>
-            <div style={{ height: 400, width: '100%' }}>
+        <div className="card">
+            <div style={{ height: 400, width: '100%' }} className="">
                 <DataGrid
-                    rows={rows}
+                    rows={produtos}
                     columns={columns}
                     pageSize={5}
                     rowsPerPageOptions={[5]}
@@ -87,4 +62,4 @@ const ListUser = () => {
 
     )
 }
-export default ListUser;
+export default ListProduto;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,15 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import AddUser from "../components/adduser/Adduser";
-import ListUser from "../components/listuser/Listuser";
+import AddUser from "../components/Adduser/Adduser";
+import ListProduto from "../components/Listuser/Listuser";
 
 const AppRouter = () =>{
     return(
         <div>
             <Router>
-                <div className="col-md-6">
-                    <h1 className="text-center" >Sephora Cosmetics</h1>
+                <div className="container">
+                    <h1 className="text-center bg-info mt-3 border p-2" >Sephora Cosmetics</h1>
                     <Routes>
-                        <Route path="/" exact element={<ListUser/>}/>
+                        <Route path="/" exact element={<ListProduto/>}/>
                         <Route path="/add" element={<AddUser/>}/>
                     </Routes>
                 </div>
